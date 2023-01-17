cae5611de75bf6580b6dcb64212310c23ae45fe9
mr socks <pawel.werbowy@gmail.com>
2023-01-17 12:55:59 +0000

    adds more code

---
+++ assets/Js/logic.js
@@ -0,0 +1,78 @@
+// variables that need to be set
+
+// var = questionIndex = 0;  // not sure if i need this yet
+
+var countdownTimer = document.querySelector('#time');  // timer 
+var secondsLeft = 90                                   // timer 
+
+var timerEl = document.querySelector("#timer");  // timer 
+var startButtonEl = document.querySelector('#start');    // start button
+var submitButtonEl = document.querySelector('#submit'); // submit button
+var startScreenEl = document.querySelector('#startScreen'); // start screen
+var questionsEl = document.querySelector('#questions'); // questions
+var choicesEl = document.querySelector('#choices'); // choices
+var questionTitlesEl = document.querySelector('#question-title'); // question title
+var endScreenEl = document.querySelector('#end-screen'); // end screen
+var finalScoreEl = document.querySelector('#final-score'); // final score
+var initialsEl = document.querySelector('#initials'); // initials
+var feedbackEl = document.querySelector('#feedback'); // feedback
+
+
+
+// clicking start button starts quiz and timer
+
+startButton.addEventListener('click', function {
+    // countdownTimer.textContent = '00:00:00';  // double check if needed 
+    startQuiz();
+    startTimer();
+    
+})
+
+// event listener added to start button 
+
+function startQuiz() {
+    startScreenEl.setAttribute('class', 'hide'); // hide start screen
+
+    
+
+};
+
+
+function startTimer() {
+    // secondsLeft--;
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+
+    }, 1000)
+};
+
+// function that starts the quiz and the timer above
+
+
+
+function nextQuestion() {
+
+};
+
+// once the quiz starts few questions need to be displayed 
+
+// each question has three choices and one answer is correct
+
+// once the choice is made the next question button is pressed next question is presented
+// if answer is incorrect 10 seconds is deducted from the timer 
+
+// when the timer reaches 0 the quiz is over 
+
+function gameOver() {
+
+};
+
+
+// score is saved in local storage
+
+function savedScore() {
+
+};
+
+function highScores() {};
---
+++ assets/Js/questions.js
@@ -0,0 +1,44 @@
+// Questions used from JavaScript Quiz on https://www.proprofs.com/quiz-school/story.php?title=pp-mzqxmzy3mwt58y
+
+var questions = [
+    {
+        title: "Which statement cannot be used to declare a variable in JavaScript?",
+        choices: ["Let", "Var", "Int"],
+        answer: "Int"
+    },
+
+    {
+        title: "What are Functions in JavaScript?",
+        choices: ["Functions are basically a statement which returns a constant value",
+                  "Functions are basically a set of statements that perform some task",
+                  "Functions are one of the data types in JavaScript"],
+        answer: "Functions are basically a set of statements that perform some task"
+    },
+
+    {
+        title: "Which of the following is the right way to add a single line comment in JavaScript?",   
+        choices: ["#This is a comment",
+                  "/This is a comment",
+                  "//This is a comment"],
+        answer: "//This is a comment"},
+
+    {
+        title: "Which array method is used to iterate on all the array elements and perform some task/transformation on them and return the new array?",   
+        choices: ["Map",
+                  "Filter",
+                  "ForEach"],
+        answer: "Map"},
+
+    {
+        title: "Which method is used to listen to events from DOM elements?",   
+        choices: ["Element.addListener(event => {})",
+                  "AddListener(element, event => {})",
+                  "Element.addEventListener(event => {})"],
+        answer: "Element.addEventListener(event => {})"},
+
+
+
+
+];
+
+// console.log(questions);
