bb7b871b70ab7513fcdd60a2a1046d1d83e7ea37
mr socks <pawel.werbowy@gmail.com>
2023-01-24 21:24:29 +0000

    fixes high scores

--- assets/Js/scores.js
+++ assets/Js/scores.js
@@ -1,2 +1,25 @@
-// console.log("These are the scores");
+
+
+
+
+
+function displayScores() {
+    
+    var listEl = document.getElementById("highscores");
+    var scores = JSON.parse(localStorage.getItem("scores"))
+
+    for (var i = 0; i < scores.length; i++) {
+        
+    var scoresListItem = document.createElement("li");
+        scoresListItem.textContent = scores[i].initials + " - " + scores[i].score;
+        
+        listEl.appendChild(scoresListItem);
+        
+
+        }
+
+
+}
+
+displayScores();
 
