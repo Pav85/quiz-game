12e2a3fa5214e859552bf363011ac6f34824b190
Pav85 <pawel.werbowy@gmail.com>
2023-02-22 10:19:27 +0000

    adds functionality to clear scores button

--- assets/Js/scores.js
+++ assets/Js/scores.js
@@ -1,25 +1,25 @@
-
-
-
-
+var clearScoresButton = document.querySelector("#clear");
+var listEl = document.getElementById("highscores");
 
 function displayScores() {
-    
-    var listEl = document.getElementById("highscores");
-    var scores = JSON.parse(localStorage.getItem("scores"))
+  //   var listEl = document.getElementById("highscores");
+  var scores = JSON.parse(localStorage.getItem("scores"));
 
-    for (var i = 0; i < scores.length; i++) {
-        
+  for (var i = 0; i < scores.length; i++) {
     var scoresListItem = document.createElement("li");
-        scoresListItem.textContent = scores[i].initials + " - " + scores[i].score;
-        
-        listEl.appendChild(scoresListItem);
-        
+    scoresListItem.textContent = scores[i].initials + " - " + scores[i].score;
 
-        }
-
-
+    listEl.appendChild(scoresListItem);
+  }
 }
 
 displayScores();
 
+function clearScores() {
+  localStorage.clear();
+}
+
+clearScoresButton.addEventListener("click", function () {
+  clearScores();
+  listEl.classList.add("hide");
+});
